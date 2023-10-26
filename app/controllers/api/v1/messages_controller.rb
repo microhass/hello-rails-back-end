8034f786717e003e57039eba4a60da66eeae5b58
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-26 15:40:37 +0300

    generate messages route

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,4 @@
-class MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
   before_action :set_message, only: %i[ show update destroy ]
 
   # GET /messages
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :messages
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
+      
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
